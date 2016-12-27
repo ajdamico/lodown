f24027f9b0e8b49f903324e869a3481a6d4fe1dd
Anthony Damico <ajdamico@gmail.com>
2016-12-27 18:25:03 -0500

    ces

---
+++ R/census_thresholds.R
@@ -0,0 +1,100 @@
+
+census_thresholds <-
+	function(){
+	
+		if ( !requireNamespace( "reshape2" , quietly = TRUE ) ) stop( "reshape2 needed for this function to work. to install it, type `install.packages( 'reshape2' )`" , call. = FALSE )
+		if ( !requireNamespace( "readxl" , quietly = TRUE ) ) stop( "readxl needed for this function to work. to install it, type `install.packages( 'readxl' )`" , call. = FALSE )
+
+		
+		# initiate an empty data.frame object
+		all_thresholds <- NULL
+
+		cpov <- "https://www.census.gov/data/tables/time-series/demo/income-poverty/historical-poverty-thresholds.html" 
+
+		pgdl <- try( pg <- xml2::read_html(cpov) , silent = TRUE )
+		if( 'try-error' %in% class( pgdl ) ) pg <- xml2::read_html(cpov, method='wininet')
+
+		all_links <- rvest::html_attr( rvest::html_nodes(pg, "a"), "href")
+
+		# find all excel files on the census poverty webpage
+		excel_locations <- grep( "thresholds/thresh(.*)\\.(.*)" , all_links , value = TRUE )
+
+		# figure out which years are available among the excel file strings
+		ya <- ( 1990:2058 )[ substr( 1990:2058 , 3 , 4 ) %in% gsub( "(.*)thresh(.*)\\.(.*)" , "\\2" , excel_locations ) ]
+
+		# loop through all years available
+		for ( year in rev(ya) ){
+
+			# figure out the location of the excel file on the drive
+			this_excel <- paste0( "https:" , grep( substr( year , 3 , 4 ) , excel_locations , value = TRUE ) )
+			
+			# name the excel file something appropriate
+			fn <- paste0( tempdir() , "/" , basename( this_excel ) )
+			
+			# download the file to your local disk
+			cachaca( this_excel , fn , mode = 'wb' )
+			
+			# import the current table
+			if( grepl( "\\.csv$" , fn ) ){
+
+				skipsix <- try( this_thresh <- read.csv( fn , skip = 6 , stringsAsFactors = FALSE ) , silent = TRUE )
+				
+				if( class( skipsix ) == 'try-error' ) skipfive <- try( this_thresh <- read.csv( fn , skip = 7 , stringsAsFactors = FALSE ) , silent = TRUE ) else skipfive <- NULL
+				
+				if( class( skipfive ) == 'try-error' ) this_thresh <- read.csv( fn , skip = 5 , stringsAsFactors = FALSE )
+				
+			} else {
+				
+				this_thresh <- readxl::read_excel( fn )
+				
+			}
+			
+			# if the text `Weighted` exists in the second column, toss the second column
+			if( any( grepl( "Weighted" , c( names( this_thresh )[2] , this_thresh[ , 2 ] ) ) ) ) this_thresh <- this_thresh[ , -2 ]
+			
+			# keep all rows where the second column is not missing
+			this_thresh <- this_thresh[ !is.na( this_thresh[ , 2 ] ) & !( stringr::str_trim( this_thresh[ , 2 ] ) == "" ) , ]
+			
+			# remove crap at the beginning and end
+			this_thresh[ , 1 ] <- stringr::str_trim( iconv( as.character( this_thresh[ , 1 ] ) , to = "ASCII" , sub = " " ) )
+			this_thresh[ , 1 ] <- stringr::str_trim( gsub( "\\." , "" , as.character( this_thresh[ , 1 ] ) ) )
+			this_thresh <- this_thresh[ !is.na( this_thresh[ , 1 ] ) & !( this_thresh[ , 1 ] %in% "" ) ,  ]
+			
+			this_thresh[ -1 ] <- sapply( this_thresh[ -1 ] , function( z ) as.numeric( gsub( ",|\\$" , "" , z ) ) )
+			
+			
+			# keep only rows where a `family_type` matches something we've already found
+			# this_thresh <- this_thresh[ this_thresh[ , 1 ] %in% all_thresholds$family_type , ]
+
+			# name the 2nd-10th columns based on number of kids
+			names( this_thresh ) <- c( "family_type" , 0:8 )
+			
+			# reshape the table from wide to long
+			this_thresh <- reshape2::melt( this_thresh , "family_type" )
+			
+			# appropriately name everything
+			names( this_thresh ) <- c( 'family_type' , 'num_kids' , 'threshold' )
+			
+			# tack on the year
+			this_thresh$year <- year
+			
+			this_thresh <- this_thresh[ !is.na( this_thresh$family_type ) & !is.na( this_thresh$threshold ) , ]
+				
+			# typo on census bureau page
+			this_thresh <- this_thresh[ !( this_thresh$threshold == 26753 & this_thresh$year == 2000 & this_thresh$num_kids == 8 & this_thresh$family_type == "Eight persons" ) , ]
+
+			stopifnot( nrow( this_thresh ) == 48 )
+			
+			# stack it with the others
+			all_thresholds <- rbind( all_thresholds , this_thresh )
+			
+		}
+
+		all_thresholds$family_type <- gsub( "persons" , "people" , all_thresholds$family_type )
+		all_thresholds$num_kids <- as.numeric( as.character( all_thresholds$num_kids ) )
+
+		all_thresholds
+
+		# done scraping official census poverty thresholds back to 1990
+	}
+	
---
+++ R/ces.R
@@ -0,0 +1,115 @@
+get_catalog_ces <-
+	function( data_name = "ces" , output_dir , ... ){
+
+	all_links <- rvest::html_attr( rvest::html_nodes( xml2::read_html( "https://www.bls.gov/cex/pumd_data.htm#stata" ) , "a" ) , "href" )
+
+	stata_links <- grep( "stata(.*)\\.zip" , all_links , value = TRUE , ignore.case = TRUE )
+	
+	stata_years <- gsub( "(.*)([0-9][0-9])(.*)" , "\\2" , basename( stata_links ) )
+	
+	catalog <-
+		data.frame(
+			type = gsub( "([0-9][0-9])(.*)" , "" , basename( stata_links ) ) ,
+			year = ifelse( as.numeric( stata_years ) > 95 , 1900 + as.numeric( stata_years ) , 2000 + as.numeric( stata_years ) ) ,
+			full_url = paste0( 'https://www.bls.gov/' , stata_links ) ,
+			stringsAsFactors = FALSE
+		)
+		
+	catalog$output_filename <- paste0( output_dir , "/" , catalog$year , "/" , catalog$type , ".rda" )
+
+	catalog
+
+}
+
+
+lodown_ces <-
+	function( catalog , data_name = "ces" , ... ){
+
+		all_thresholds <- census_thresholds()
+	
+		tf <- tempfile()
+
+		for ( i in seq_len( nrow( catalog ) ) ){
+
+			# download the file
+			cachaca( catalog[ i , "full_url" ] , tf , mode = 'wb' )
+
+			unzipped_files <- unzip( tf , exdir = paste0( tempdir() , "/unzips" ) )
+
+			# identify dta files
+			dta_files <- unzipped_files[ grep( '\\.dta' , unzipped_files ) ]
+
+			df_names <- NULL
+			
+			# loop through a character vector containing the complete filepath
+			# of each of the dta files downloaded to the local disk..
+			for ( this_dta in dta_files ){
+
+				df_name <- gsub( "(.*)\\.(.*)" , "\\1" , basename( this_dta ) )
+			
+				# read the current stata-readable (.dta) file into R
+				x <- foreign::read.dta( this_dta )
+
+				# if the data.frame is a family file, tack on poverty thresholds
+				if( grepl( "fmli" , df_name ) ){
+
+					# subset the complete threshold data down to only the current year
+					thresh_merge <- all_thresholds[ all_thresholds$this_year == catalog[ i , "year" ] , ]
+
+					# remove the `year` column
+					thresh_merge$this_year <- NULL
+
+					# rename fields so they merge cleanly
+					names( thresh_merge ) <- c( 'family_type' , 'num_kids' , 'poverty_threshold' )
+
+					x$num_kids <- ifelse( x$perslt18 > 8 , 8 , x$perslt18 )
+					x$num_kids <- ifelse( x$num_kids == x$fam_size , x$fam_size - 1 , x$num_kids )
+
+					# re-categorize family sizes to match census groups
+					x$family_type <-
+						ifelse( x$fam_size == 1 & x$age_ref < 65 , "Under 65 years" ,
+						ifelse( x$fam_size == 1 & x$age_ref >= 65 , "65 years and over" ,
+						ifelse( x$fam_size == 2 & x$age_ref < 65 , "Householder under 65 years" ,
+						ifelse( x$fam_size == 2 & x$age_ref >= 65 , "Householder 65 years and over" ,
+						ifelse( x$fam_size == 3 , "Three people" , 
+						ifelse( x$fam_size == 4 , "Four people" , 
+						ifelse( x$fam_size == 5 , "Five people" , 
+						ifelse( x$fam_size == 6 , "Six people" , 
+						ifelse( x$fam_size == 7 , "Seven people" , 
+						ifelse( x$fam_size == 8 , "Eight people" , 
+						ifelse( x$fam_size >= 9 , "Nine people or more" , NA ) ) ) ) ) ) ) ) ) ) )
+
+					# merge on the `poverty_threshold` variable while
+					# confirming no records were tossed
+					before_nrow <- nrow( x )
+
+					x <- merge( x , thresh_merge )
+
+					stopifnot( nrow( x ) == before_nrow )
+
+				}
+
+				# convert all column names to lowercase
+				names( x ) <- tolower( names( x ) )
+
+				# save it to an object named by what's contained in the df.name character string
+				assign( df_name , x )
+
+				df_names <- unique( c( df_names , df_name ) )
+				
+			}
+				
+			# save the file as an R data file (.rda) immediately
+			save( list = df_names , file = catalog[ i , 'output_filename' ] )
+
+			# delete the temporary files
+			file.remove( tf , unzipped_files )
+
+			cat( paste0( data_name , " catalog entry " , i , " of " , nrow( catalog ) , " stored at '" , catalog[ i , 'output_filename' ] , "'\r\n\n" ) )
+
+		}
+
+		invisible( TRUE )
+
+	}
+
