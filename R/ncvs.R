a0adbc647fc74dde1f9efd8f14a019c5c078e083
Anthony Damico <ajdamico@gmail.com>
2016-12-29 13:32:27 -0500

    ncvs cleanups

--- R/ncvs.R
+++ R/ncvs.R
@@ -1,9 +1,9 @@
 get_catalog_ncvs <-
 	function( data_name = "ncvs" , output_dir , ... ){
 
-	catalog <- get_catalog_icpsr( "95" , archive = "NACJD" )
+	catalog <- get_catalog_icpsr( "95" , archive = "NACJD" , bundle_preference = c( "rdata" , "stata" , "ascsas" ) )
 	
-	catalog$unzip_folder <- paste0( output_dir , "/" , gsub( "\\[|\\]" , "" , gsub( "[^0-9A-z ]" , "" , gsub( "-" , " " , catalog$name ) ) , "/" , catalog$dataset_name ) )
+	catalog$unzip_folder <- paste0( output_dir , "/" , gsub( "\\[|\\]" , "" , gsub( "[^0-9A-z ]" , "" , gsub( "-" , " " , catalog$name ) ) , "/" , catalog$dataset_name , "/" ) )
 
 	catalog$db_tablename <- tolower( gsub( " |-" , "_" , paste0( gsub( "([^0-9A-z ])" , "" , paste( catalog$name , catalog$dataset_name ) ) ) ) )
 	catalog$db_tablename <- gsub( "national_crime_victimization_survey_" , "x" , catalog$db_tablename )
@@ -28,23 +28,40 @@ lodown_ncvs <-
 		
 		for( i in seq_len( nrow( catalog ) ) ){
 		
-			# find stata file within unzipped path
-			stata_files <- grep( "\\.dta$" , list.files( catalog[ i , 'unzip_folder' ] , full.names = TRUE ) , value = TRUE )
-			
-			stopifnot( length( stata_files ) > 1 )
-			
-			if( length( stata_files ) == 1 ){
+			if( grepl( "bundle=stata" , catalog[ i , 'full_url' ] ) ){
+		
+				# find stata file within unzipped path
+				stata_files <- grep( "\\.dta$" , list.files( catalog[ i , 'unzip_folder' ] , full.names = TRUE ) , value = TRUE )
+				
+				stopifnot( length( stata_files ) == 1 )
 				
 				x <- icpsr_stata( x , catalog_entry = catalog[ i , ] )
 
+			}
+			
+			if( grepl( "bundle=ascsas" , catalog[ i , 'full_url' ] ) ){
+		
+				# find stata file within unzipped path
+				txt <- grep( "\\.txt$" , list.files( catalog[ i , 'unzip_folder' ] , full.names = TRUE ) , value = TRUE )
+				
+				sas <- grep( "\\.sas$" , list.files( catalog[ i , 'unzip_folder' ] , full.names = TRUE ) , value = TRUE )
+				
+				stopifnot( length( stata_files ) == 1 )
+				
+				x <- read_SAScii( txt , sas )
+
+			}
+			
+			if( !grepl( "bundle=rdata" , catalog[ i , 'full_url' ] ) ){
+				
 				names( x ) <- tolower( names( x ) )
 				
 				save( x , file = catalog[ i , 'output_filename' ] )
 				
 				cat( paste0( data_name , " catalog entry " , i , " of " , nrow( catalog ) , " stored at '" , catalog[ i , 'output_filename' ] , "'\r\n\n" ) )
-			
+
 			}
-		
+			
 		}
 
 		invisible( TRUE )
