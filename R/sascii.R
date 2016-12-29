d098aa2d0c6843dbcba00487004c9322a364820d
Anthony Damico <ajdamico@gmail.com>
2016-12-29 16:04:11 -0500

    )

--- R/sascii.R
+++ R/sascii.R
@@ -31,7 +31,7 @@ read_SAScii <-
 			
 			options(scipen = 1e+06)
 			
-			no_decimal_points <- unlist( sapply( x , function( z ) ( sum( grepl( "." , z , fixed = TRUE ) ) == 0 ) )
+			no_decimal_points <- unlist( sapply( x , function( z ) ( sum( grepl( "." , z , fixed = TRUE ) ) == 0 ) ) )
 			
 			cols_to_multiply <- no_decimal_points & !y[ , "char" ] & y[ , "divisor" ] != 1
 			
