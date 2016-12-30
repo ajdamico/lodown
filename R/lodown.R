b64ee5d9c59ef5a7a4b60008c863abf3e8583384
Anthony Damico <ajdamico@gmail.com>
2016-12-30 10:19:37 -0500

    nbs

--- R/lodown.R
+++ R/lodown.R
@@ -28,6 +28,7 @@ NULL
 #' lodown( "ces" , output_dir = "C:/My Directory/CES" )
 #' lodown( "ess" , output_dir = "C:/My Directory/ESS" , your_email = "email@address.com" )
 #' lodown( "faers" , output_dir = "C:/My Directory/FAERS" )
+#' lodown( "nbs" , output_dir = "C:/My Directory/NBS" )
 #' lodown( "ncvs" , output_dir = "C:/My Directory/NCVS" ,
 #' 		your_email = "email@address.com" , your_password = "password" )
 #' lodown( "nhis" , output_dir = "C:/My Directory/NHIS" )
@@ -68,6 +69,8 @@ NULL
 #' lodown( "ess" , ess_cat[ 1:2 , ] , your_email = "email@address.com" )
 #' faers_cat <- get_catalog( "faers" , output_dir = "C:/My Directory/FAERS" )
 #' lodown( "faers" , faers_cat[ 1:2 , ] )
+#' nbs_cat <- get_catalog( "nbs" , output_dir = "C:/My Directory/NBS" )
+#' lodown( "nbs" , nbs_cat[ 1:2 , ] )
 #' ncvs_cat <- get_catalog( "ncvs" , output_dir = "C:/My Directory/NCVS" )
 #' lodown( "ncvs" , ncvs_cat[ 1:2 , ] ,
 #' 		your_email = "email@address.com" , your_password = "password" )
---
+++ R/nbs.R
@@ -0,0 +1,71 @@
+get_catalog_nbs <-
+	function( data_name = "nbs" , output_dir , ... ){
+
+		catalog <- NULL
+		
+		puf_homepage <- "https://www.ssa.gov/disabilityresearch/publicusefiles.html"
+	
+		nbs_rounds <- gsub( "(.*)nbs_round_(.*)\\.htm(.*)" , "nbs_round_\\2.htm" , grep( "nbs_round" , readLines( puf_homepage , warn = FALSE ) , value = TRUE ) )
+		
+		for( this_round in nbs_rounds ){
+			
+			this_csv_line <- grep( "href(.*)\\.csv" , readLines( paste0( "https://www.ssa.gov/disabilityresearch/" , this_round ) , warn = FALSE ) , ignore.case = TRUE , value = TRUE )[ 1 ]
+	
+			this_csv <- paste0( "https://www.ssa.gov/disabilityresearch/" , gsub( '(.*)"(.*)"(.*)' , "\\2" , this_csv_line ) )
+	
+			round_number <- gsub( "nbs_round_(.*)\\.htm" , "\\1" , this_round )
+	
+			catalog <-
+				rbind(
+					catalog ,
+					data.frame(
+						this_round = round_number ,
+						full_url = this_csv ,
+						output_filename = paste0( output_dir , "/" , "round " , stringr::str_pad( round_number , 2 , pad = "0" ) , ".rda" ) ,
+						stringsAsFactors = FALSE
+					)
+				)
+				
+		}
+
+		catalog
+
+	}
+
+
+lodown_nbs <-
+	function( data_name = "nbs" , catalog , ... ){
+
+		tf <- tempfile()
+
+
+		for ( i in seq_len( nrow( catalog ) ) ){
+
+			# download the file
+			cachaca( catalog[ i , "full_url" ] , tf , mode = 'wb' , filesize_fun = 'httr' )
+
+			x <- read.csv( tf , stringsAsFactors = FALSE )
+			
+			# convert all column names to lowercase
+			names( x ) <- tolower( names( x ) )
+			
+			# remove those rounds at the front of the column names
+			names( x ) <- gsub( paste0( "^r" , i , "_" ) , "" , names( x ) )
+			# that `^` symbol instructs r to only match a pattern at the start of the string.
+			
+			# add a column of nuthin' but ones.
+			x$one <- 1
+			
+			save( x , file = catalog[ i , 'output_filename' ] )
+
+			# delete the temporary files
+			suppressWarnings( file.remove( tf ) )
+
+			cat( paste0( data_name , " catalog entry " , i , " of " , nrow( catalog ) , " stored at '" , catalog[ i , 'output_filename' ] , "'\r\n\n" ) )
+
+		}
+
+		invisible( TRUE )
+
+	}
+
