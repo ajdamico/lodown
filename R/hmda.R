267b8d84deb76898b76260a3a7c9e9404d88a2d9
Anthony Damico <ajdamico@gmail.com>
2016-12-31 17:09:14 -0500

    race ethnicity

--- R/hmda.R
+++ R/hmda.R
@@ -22,6 +22,7 @@ get_catalog_hmda <-
 				year = 2006:latest_pmic_year ,
 				type = 'pmic_lar' ,
 				full_url = paste0( "https://www.ffiec.gov/" , "pmic" , "rawdata/" , "LAR/National" , "/" , 2006:latest_pmic_year , "PMIC" , "lar%20-%20National" , ".zip" ) ,
+				merge_table = paste0( 'pmic_' , 2006:latest_pmic_year ) ,
 				stringsAsFactors = FALSE
 			)
 			
@@ -38,6 +39,7 @@ get_catalog_hmda <-
 				year = 2006:latest_pmic_year ,
 				type = 'pmic_inst' ,
 				full_url = paste0( "https://www.ffiec.gov/" , "pmic" , "rawdata/" , "OTHER" , "/" , 2006:latest_pmic_year , "PMIC" , "institutionrecords" , ".zip" ) ,
+				merge_table = paste0( 'pmic_' , 2006:latest_pmic_year ) ,
 				stringsAsFactors = FALSE
 			)
 			
@@ -46,6 +48,7 @@ get_catalog_hmda <-
 				year = 2007:latest_hmda_year ,
 				type = 'hmda_reporter' ,
 				full_url = paste0( "https://www.ffiec.gov/" , "hmda" , "rawdata/OTHER/" , 2007:latest_hmda_year , "HMDA" , "ReporterPanel.zip" ) ,
+				merge_table = NA ,
 				stringsAsFactors = FALSE
 			)
 			
@@ -54,6 +57,7 @@ get_catalog_hmda <-
 				year = 2007:latest_pmic_year ,
 				type = 'pmic_reporter' ,
 				full_url = paste0( "https://www.ffiec.gov/" , "pmic" , "rawdata/OTHER/" , 2007:latest_pmic_year , "PMIC" , "ReporterPanel.zip" ) ,
+				merge_table = NA ,
 				stringsAsFactors = FALSE
 			)
 			
@@ -63,6 +67,7 @@ get_catalog_hmda <-
 				year = 2007:latest_hmda_year ,
 				type = 'hmda_msa' ,
 				full_url = paste0( "https://www.ffiec.gov/" , "hmda" , "rawdata/OTHER/" , 2007:latest_hmda_year , "HMDA" , "MSAOffice.zip" ) ,
+				merge_table = NA ,
 				stringsAsFactors = FALSE
 			)
 			
@@ -71,6 +76,7 @@ get_catalog_hmda <-
 				year = 2007:latest_pmic_year ,
 				type = 'pmic_msa' ,
 				full_url = paste0( "https://www.ffiec.gov/" , "hmda" , "rawdata/OTHER/" , 2007:latest_pmic_year , "HMDA" , "MSAOffice.zip" ) ,
+				merge_table = NA ,
 				stringsAsFactors = FALSE
 			)
 			
@@ -280,9 +286,6 @@ lodown_hmda <-
 			}
 			
 			
-
-
-			
 			stopifnot( DBI::dbGetQuery( db , paste( 'select count(*) from' , catalog[ i , 'db_tablename' ] ) ) > 0 )
 
 			# disconnect from the current monet database
@@ -295,6 +298,208 @@ lodown_hmda <-
 
 		}
 
+		
+		unique_merge_tables <- unique( catalog$merge_table )
+		
+		for( i in seq_along( unique_merge_tables ) ){
+
+			ins.tablename <- catalog[ catalog$type == 'ins' & catalog$merge_table == unique_merge_tables[ i ] , 'db_tablename' ]
+			lar.tablename <- catalog[ catalog$type == 'lar' & catalog$merge_table == unique_merge_tables[ i ] , 'db_tablename' ]
+			new.tablename <- unique_merge_tables[ i ]
+			
+			# three easy steps #
+			
+			# step one: confirm the only intersecting fields are "respondentid" and "agencycode"
+			# these are the merge fields, so nothing else can overlap
+			
+			stopifnot( 
+				identical( 
+					intersect( 
+						DBI::dbListFields( db , lar.tablename ) , 
+						DBI::dbListFields( db , ins.tablename ) 
+					) , 
+					c( 'respondentid' , 'agencycode' ) 
+				) 
+			)
+			
+			# step two: merge the two tables
+			
+			# extract the column names from the institution table
+			ins.fields <- DBI::dbListFields( db , ins.tablename )
+			
+			# throw out the two merge fields
+			ins.nomatch <- ins.fields[ !( ins.fields %in% c( 'respondentid' , 'agencycode' ) ) ]
+			
+			# add a "b." in front of every field name
+			ins.b <- paste0( "b." , ins.nomatch )
+			
+			# separate all of them by commas into a single character string
+			ins.string <- paste( ins.b , collapse = ", " )
+			
+			# construct the merge command
+			sql.merge.command <-
+				paste(
+					"CREATE TABLE" , 
+					new.tablename ,
+					"AS SELECT a.* ," ,
+					ins.string ,
+					"FROM" ,
+					lar.tablename ,
+					"AS a INNER JOIN" ,
+					ins.tablename ,
+					"AS b ON a.respondentid = b.respondentid AND a.agencycode = b.agencycode WITH DATA"
+				)
+			
+			# with your sql string built, execute the command
+			DBI::dbSendQuery( db , sql.merge.command )
+			
+			# step three: confirm that the merged table contains the same record count
+			stopifnot( 
+				DBI::dbGetQuery( 
+					db , 
+					paste(
+						'select count(*) from' ,
+						new.tablename
+					)
+				) ==
+				DBI::dbGetQuery( 
+					db , 
+					paste(
+						'select count(*) from' ,
+						lar.tablename
+					)
+				)
+			)
+
+			# # # # # # # # # # # # # # # # # #
+			# # race and ethnicity recoding # #
+			# # # # # # # # # # # # # # # # # #
+			
+			# number of minority races of applicant and co-applicant
+			DBI::dbSendQuery( db , paste( 'ALTER TABLE' , new.tablename , 'ADD COLUMN app_min_cnt INTEGER' ) )
+			DBI::dbSendQuery( db , paste( 'ALTER TABLE' , new.tablename , 'ADD COLUMN co_min_cnt INTEGER' ) )
+
+			# sum up all four possibilities
+			DBI::dbSendQuery( 
+				db , 
+				paste(
+					'UPDATE' ,
+					new.tablename ,
+					'SET 
+						app_min_cnt = 
+						(
+							( applicantrace1 IN ( 1 , 2 , 3 , 4 ) )*1 +
+							( applicantrace2 IN ( 1 , 2 , 3 , 4 ) )*1 +
+							( applicantrace3 IN ( 1 , 2 , 3 , 4 ) )*1 +
+							( applicantrace4 IN ( 1 , 2 , 3 , 4 ) )*1 +
+							( applicantrace5 IN ( 1 , 2 , 3 , 4 ) )*1
+						)' 
+				)
+			)
+
+			# same for the co-applicant
+			DBI::dbSendQuery( 
+				db , 
+				paste(
+					'UPDATE' ,
+					new.tablename , 
+					'SET 
+						co_min_cnt = 
+						(
+							( coapplicantrace1 IN ( 1 , 2 , 3 , 4 ) )*1 +
+							( coapplicantrace2 IN ( 1 , 2 , 3 , 4 ) )*1 +
+							( coapplicantrace3 IN ( 1 , 2 , 3 , 4 ) )*1 +
+							( coapplicantrace4 IN ( 1 , 2 , 3 , 4 ) )*1 +
+							( coapplicantrace5 IN ( 1 , 2 , 3 , 4 ) )*1
+						)' 
+				)
+			)
+
+			# zero-one test of whether the applicant or co-applicant indicated white
+			DBI::dbSendQuery( db , paste( 'ALTER TABLE' , new.tablename , 'ADD COLUMN appwhite INTEGER' ) )
+			DBI::dbSendQuery( db , paste( 'ALTER TABLE' , new.tablename , 'ADD COLUMN cowhite INTEGER' ) )
+
+			# check all five race categories for the answer
+			DBI::dbSendQuery( 
+				db , 
+				paste(
+					'UPDATE' ,
+					new.tablename , 
+					'SET 
+						appwhite = 
+						( 
+							( ( applicantrace1 ) IN ( 5 ) )*1 + 
+							( ( applicantrace2 ) IN ( 5 ) )*1 + 
+							( ( applicantrace3 ) IN ( 5 ) )*1 + 
+							( ( applicantrace4 ) IN ( 5 ) )*1 + 
+							( ( applicantrace5 ) IN ( 5 ) )*1 
+						)' 
+				)
+			)
+
+			# same for the co-applicant
+			DBI::dbSendQuery( 
+				db , 
+				paste(
+					'UPDATE' ,
+					new.tablename ,
+					'SET 
+						cowhite = 
+						( 
+							( ( coapplicantrace1 ) IN ( 5 ) )*1 + 
+							( ( coapplicantrace2 ) IN ( 5 ) )*1 + 
+							( ( coapplicantrace3 ) IN ( 5 ) )*1 + 
+							( ( coapplicantrace4 ) IN ( 5 ) )*1 + 
+							( ( coapplicantrace5 ) IN ( 5 ) )*1 
+						)' 
+				)
+			)
+
+			# if the applicant or co-applicant has a missing first race, set the above variables to missing as well
+			DBI::dbSendQuery( db , paste( 'UPDATE' , new.tablename , 'SET app_min_cnt = NULL WHERE applicantrace1 IN ( 6 , 7 )' ) )
+			DBI::dbSendQuery( db , paste( 'UPDATE' , new.tablename , 'SET appwhite = NULL WHERE applicantrace1 IN ( 6 , 7 )' ) )
+			DBI::dbSendQuery( db , paste( 'UPDATE' , new.tablename , 'SET co_min_cnt = NULL WHERE coapplicantrace1 IN ( 6 , 7 , 8 )' ) )
+			DBI::dbSendQuery( db , paste( 'UPDATE' , new.tablename , 'SET cowhite = NULL WHERE coapplicantrace1 IN ( 6 , 7 , 8 )' ) )
+
+			# main race variable
+			DBI::dbSendQuery( db , paste( 'ALTER TABLE' , new.tablename , 'ADD COLUMN race INTEGER' ) )
+
+			# 7 indicates a loan by a white applicant and non-white co-applicant or vice-versa
+			DBI::dbSendQuery( db , paste( 'UPDATE' , new.tablename , 'SET race = 7 WHERE ( appwhite = 1 AND app_min_cnt = 0 AND co_min_cnt > 0 ) OR ( cowhite = 1 AND co_min_cnt = 0 AND app_min_cnt > 0 )' ) )
+
+			# 6 indicates the main applicant listed multiple non-white races
+			DBI::dbSendQuery( db , paste( 'UPDATE' , new.tablename , 'SET race = 6 WHERE ( app_min_cnt > 1 ) AND ( race IS NULL )' ) )
+
+			# for everybody else: if the first race listed by the applicant isn't white, use that.
+			DBI::dbSendQuery( db , paste( "UPDATE" , new.tablename , "SET race = applicantrace1 WHERE ( applicantrace1 IN ( '1' , '2' , '3' , '4' ) ) AND ( app_min_cnt = 1 ) AND ( race IS NULL )" ) )
+			# otherwise look to the second listed race
+			DBI::dbSendQuery( db , paste( "UPDATE" , new.tablename , "SET race = applicantrace2 WHERE ( applicantrace2 IN ( '1' , '2' , '3' , '4' ) ) AND ( app_min_cnt = 1 ) AND ( race IS NULL )" ) )
+			# otherwise confirm the applicant indicated he or she was white
+			DBI::dbSendQuery( db , paste( 'UPDATE' , new.tablename , "SET race = 5 WHERE ( appwhite = 1 ) AND ( race IS NULL )" ) )
+
+			# main ethnicity variable
+			DBI::dbSendQuery( db , paste( 'ALTER TABLE' , new.tablename , 'ADD COLUMN ethnicity VARCHAR (255)' ) )
+
+			# simple.  check the applicant's ethnicity
+			DBI::dbSendQuery( db , paste( "UPDATE" , new.tablename , "SET ethnicity = 'Not Hispanic' WHERE applicantethnicity IN ( 2 )" ) )
+			
+			# simple.  check the applicant's ethnicity again
+			DBI::dbSendQuery( db , paste( "UPDATE" , new.tablename , "SET ethnicity = 'Hispanic' WHERE applicantethnicity IN ( 1 )" ) )
+			
+			# overwrite the ethnicity variable if the main applicant indicates hispanic but the co-applicant does not.  or vice versa.
+			DBI::dbSendQuery( db , paste( "UPDATE" , new.tablename , "SET ethnicity = 'Joint' WHERE ( applicantethnicity IN ( 1 ) AND coapplicantethnicity IN ( 2 ) ) OR ( applicantethnicity IN ( 2 ) AND coapplicantethnicity IN ( 1 ) )" ) )
+
+			# # # # # # # # # # # # # # # # # # # # # # # # #
+			# # finished with race and ethnicity recoding # #
+			# # # # # # # # # # # # # # # # # # # # # # # # #
+			
+			
+			
+		}
+			
+			
+			
+		
 		invisible( TRUE )
 
 	}
