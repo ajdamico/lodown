baba795a050bf7cb0e76e86d01347803a5e6bb11
Anthony Damico <ajdamico@gmail.com>
2016-12-31 16:34:56 -0500

    close

--- R/hmda.R
+++ R/hmda.R
@@ -96,7 +96,37 @@ lodown_hmda <-
 		rp_sas <- system.file("extdata", "Reporter_Panel_2010.sas", package = "lodown")
 		pr_str <- system.file("extdata", "Reporter_Panel_Pre-2010.sas", package = "lodown")
 
-		
+		# read in the loan application record structure file
+		lar_str <- read.csv( lar_sas )
+		# paste all rows together into single strings
+		lar_col <- apply( lar_str , 1 , paste , collapse = " " )
+
+		# take a look at the `lar_str` and `lar_col` objects if you're curious
+		# just type 'em into the console to see what i mean ;)
+
+		# read in the loan application record structure file
+		ins_str <- read.csv( ins_sas )
+		# paste all rows together into single strings
+		ins_col <- apply( ins_str , 1 , paste , collapse = " " )
+
+				
+		# create an msa office sas importation script..
+		office.lines <- 
+			"INPUT
+			AS_OF_YEAR 4
+			Agency_Code $ 1
+			Respondent_ID $ 10
+			MSA_MD $ 5
+			MSA_MD_Description $ 50 
+			;"
+			
+		# ..save it to the local disk as a temporary file..
+		writeLines( office.lines , tf )
+
+		# ..and save the column names into a new object `office.names`
+		office.names <- tolower( parse.SAScii( tf )$varname )
+
+
 
 		for ( i in seq_len( nrow( catalog ) ) ){
 
@@ -116,15 +146,17 @@ lodown_hmda <-
 				files_before <- list.files( paste0( tempdir() , "/unzips" ) , full.names = TRUE )
 
 				# build the string to send to the terminal on non-windows systems
-				dos_command <- paste0( '"' , path.to.7z , '" x ' , tf , ' -aoa -o"' , paste0( tempdir() , "/unzips" ) , '"' )
+				dos_command <- paste0( '"' , path_to_7za , '" x ' , tf , ' -aoa -o"' , paste0( tempdir() , "/unzips" ) , '"' )
 
-				system( dos.command )
+				system( dos_command )
 
 				unzipped_files <- list.files( paste0( tempdir() , "/unzips" ) , full.names = TRUE )
 
-				unzipped_files <- unzipped_files[ !( unzipped_files %in% files.before ) ]
+				unzipped_files <- unzipped_files[ !( unzipped_files %in% files_before ) ]
 				
 			}
+			
+			if( length( unzipped_files ) != 1 ) stop( "always expecting a single import file" )
 
 			if( grepl( "reporter" , catalog[ i , 'type' ] ) ){
 		
