ed86c97de6574b0e693d102a7e34ef4e7b5985dd
Guilherme Jacob <guilhermejacob91@gmail.com>
2017-01-05 14:18:46 -0200

    get catalog from webpage

--- R/censo_escolar.R
+++ R/censo_escolar.R
@@ -6,22 +6,29 @@ get_catalog_censo_escolar <-
 		portal_table <- rvest::html_table( xml2::read_html( inep_portal ) , fill = TRUE )[[1]]
 
 		year_lines <- portal_table[ portal_table$X1 == "Microdados Censo Escolar" , 'X2' ]
-		
+
 		year_lines <- iconv( year_lines , "" , "ASCII" , sub = " " )
 
 		year_lines <- gsub( " +" , " " , year_lines )
 
 		censo_escolar_years <- strsplit( year_lines , " " )[[1]]
-	
+
 		catalog <-
 			data.frame(
 				year = censo_escolar_years ,
-				full_url = paste0( "http://download.inep.gov.br/microdados/micro_censo_escolar" , censo_escolar_years , ".zip" ) ,
+				full_url = NA ,
 				db_tablename = paste0( "x" , censo_escolar_years ) ,
 				dbfolder = paste0( output_dir , "/MonetDB" ) ,
 				output_folder = paste0( output_dir , "/" , censo_escolar_years ) ,
 				stringsAsFactors = FALSE
 			)
+
+		# get real full_urls
+		w <- rvest::html_attr(
+		  rvest::html_nodes( xml2::read_html( "http://portal.inep.gov.br/basica-levantamentos-acessar" ) , "a" ) , "href" )
+
+		censoesc_files <- grep( "censo_escolar.*zip$" , w , value = TRUE )
+		catalog$full_url <- censoesc_files
 
 		catalog
 
@@ -29,10 +36,13 @@ get_catalog_censo_escolar <-
 
 
 lodown_censo_escolar <-
-	function( data_name = "censo_escolar" , catalog , ... ){
+	function( data_name = "censo_escolar" , catalog , path_to_7za = if( .Platform$OS.type != 'windows' ) '7za' else normalizePath( "C:/Program Files/7-zip/7z.exe" ) , ... ){
+
+	  if( system( paste0( '"' , path_to_7za , '" -h' ) , show.output.on.console = FALSE ) != 0 ) stop( paste0( "you need to install 7-zip.  if you already have it, include a parameter like path_to_7za='" , path_to_7za , "'" ) )
+
+	  if ( !requireNamespace( "gdata" , quietly = TRUE ) ) stop( "gdata needed for this function to work. to install it, type `install.packages( 'gdata' )`" , call. = FALSE )
 
 		tf <- tempfile()
-
 
 		for ( i in seq_len( nrow( catalog ) ) ){
 
@@ -44,8 +54,32 @@ lodown_censo_escolar <-
 
 			unzipped_files <- unzip( tf , exdir = paste0( tempdir() , "/unzips" ) )
 
+			rar_files <- grep( ".rar$", unzipped_files, value = TRUE )
+
+			for( table.data in c( "docente", "matricula", "turma", "escola" ) ) {
+
+			  tabelas <- grep( table.data, rar_files, value = TRUE, ignore.case = TRUE )
+
+			  for ( j in length( tabelas ) ) {
+			    td2 <- paste0( tempdir(), "\\temptable" )
+			    dir.create( td2 )
+
+			    # build the string to send to DOS
+			    dos.command <- paste0( '"' , path_to_7za , '" x "' , normalizePath( tabelas[j] ) , '" -o"' , normalizePath( paste0( tempdir() , '\\unzips' ) ) , '"' )
+
+			    # extract the file
+			    system( dos.command , show.output.on.console = TRUE )
+
+			    # find the name of the final ASCII data file to be imported
+			    curfile <- paste0( tempdir() , '/unzips/' , gsub( ".7z" , ".txt" , basename( data.file ) ) )
+
+			    z2 <- unzip( tabelas[j], exdir = td2 )
+			  }
+
+			}
+
 			# now the files are unzipped.. read them into the monetdb table using tablename
-			
+
 				# catalog[ i , 'db_tablename' ]
 
 
