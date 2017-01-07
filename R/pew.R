70ef6fb62f9df47257fda496a58003fad6cb9935
Anthony Damico <ajdamico@gmail.com>
2017-01-07 04:58:10 -0500

    try that again

--- R/pew.R
+++ R/pew.R
@@ -87,7 +87,7 @@ get_catalog_pew <-
 
 		catalog$output_folder <- paste0( output_dir , "/" , catalog$topic , "/" , catalog$year , "/" , catalog$name , "/" )
 		
-		catalog$output_folder <- gsub( "[^A-Za-z0-9_- &]" , "_" , catalog$output_folder )
+		catalog$output_folder <- gsub( "[[:punct:]]" , "_" , catalog$output_folder )
 		
 		catalog
 
