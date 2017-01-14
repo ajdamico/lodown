0597b82567124684dbe9c1782b520369ef467262
Anthony Damico <ajdamico@gmail.com>
2017-01-14 17:17:45 -0500

    dcs

--- R/nvss.R
+++ R/nvss.R
@@ -923,11 +923,11 @@ nchs_download <-
 		
 		on.exit( unlink( tf ) )
 		
-		dir.create( y$name )
+		dir.create( paste0( output_dir , "/" , y$name ) )
 		
-		dir.create( paste( y$name , "us" , sep = "/" ) )
+		dir.create( paste( output_dir , y$name , "us" , sep = "/" ) )
 		
-		dir.create( paste( y$name , "ps" , sep = "/" ) )
+		dir.create( paste( output_dir , y$name , "ps" , sep = "/" ) )
 		
 		for ( i in c( y$us , y$ps ) ){
 			
