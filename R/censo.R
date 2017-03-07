501482afa17d1798c0b8b1784a43b06092fe7fe4
Anthony Damico <ajdamico@gmail.com>
2017-03-07 14:41:37 -0500

    Revert "allow user to pass in encoding parameter"
    This reverts commit ca94d63f09d43639f0b81131f6dc6756c0359d2c.

--- R/censo.R
+++ R/censo.R
@@ -469,7 +469,7 @@ lodown_censo <-
 # from any data files that have  #
 # been downloaded from ibge      #
 ranc_censo <- 
-	function( infiles , width , encoding = getOption("encoding") ){
+	function( infiles , width ){
 
 		tf_a <- tempfile()
 
@@ -479,7 +479,7 @@ ranc_censo <-
 		# loop through them all!
 		for ( infile in infiles ){
 
-			incon <- file( infile , "r" , encoding = encoding )
+			incon <- file( infile , "r")
 
 			line.num <- 0
 			
