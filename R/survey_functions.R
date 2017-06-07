d7f34ff131f2ad719a5c85c43004520b890b983c
Guilherme Jacob <guilhermejacob91@gmail.com>
2017-06-06 21:10:24 -0300

    svychisq for multiply-imputated dataset

--- R/survey_functions.R
+++ R/survey_functions.R
@@ -1,5 +1,5 @@
 
-	
+
 # svyttest() variant (code from the `survey` package)
 # that works on multiply-imputed data
 MIsvyttest<-function(formula, design ,...){
@@ -8,13 +8,13 @@ MIsvyttest<-function(formula, design ,...){
 	m <- eval(bquote(mitools::MIcombine( with( design , survey::svyglm(formula,family=gaussian()))) ) )
 
 	rval<-list(statistic=coef(m)[2]/survey::SE(m)[2],
-			   parameter=m$df[2],		
+			   parameter=m$df[2],
 			   estimate=coef(m)[2],
 			   null.value=0,
 			   alternative="two.sided",
 			   method="Design-based t-test",
 			   data.name=deparse(formula))
-			   
+
 	rval$p.value <- ( 1 - pf( ( rval$statistic )^2 , 1 , m$df[2] ) )
 
 	names(rval$statistic)<-"t"
@@ -24,20 +24,20 @@ MIsvyttest<-function(formula, design ,...){
 	class(rval)<-"htest"
 
 	return(rval)
-  
+
 }
 
 
 
 MIsvyciprop <-
-	function (formula, design, method = c("logit", "likelihood", 
-		"asin", "beta", "mean", "xlogit"), level = 0.95, df = mean(unlist(lapply(design$designs,survey::degf))), 
-		...) 
+	function (formula, design, method = c("logit", "likelihood",
+		"asin", "beta", "mean", "xlogit"), level = 0.95, df = mean(unlist(lapply(design$designs,survey::degf))),
+		...)
 	{
 		method <- match.arg(method)
 		if (method == "mean") {
 			m <- eval(bquote(mitools::MIcombine(with(design,survey::svymean(~as.numeric(.(formula[[2]])),...)))))
-			ci <- as.vector(confint(m, 1, level = level, df = df, 
+			ci <- as.vector(confint(m, 1, level = level, df = df,
 				...))
 			rval <- coef(m)[1]
 			attr(rval, "var") <- vcov(m)
@@ -46,7 +46,7 @@ MIsvyciprop <-
 			m <- eval(bquote(mitools::MIcombine(with(design,survey::svymean(~as.numeric(.(formula[[2]])), ...)))))
 			m <- structure(coef(m), .Names = "1", var = m$variance[1], .Dim = c(1L, 1L), statistic = "mean", class = "svystat")
 			xform <- survey::svycontrast(m, quote(asin(sqrt(`1`))))
-			ci <- sin(as.vector(confint(xform, 1, level = level, 
+			ci <- sin(as.vector(confint(xform, 1, level = level,
 				df = df, ...)))^2
 			rval <- coef(m)[1]
 			attr(rval, "var") <- vcov(m)
@@ -55,7 +55,7 @@ MIsvyciprop <-
 			m <- eval(bquote(mitools::MIcombine(with(design,survey::svymean(~as.numeric(.(formula[[2]])), ...)))))
 			m <- structure(coef(m), .Names = "1", var = m$variance[1], .Dim = c(1L, 1L), statistic = "mean", class = "svystat")
 			xform <- survey::svycontrast(m, quote(log(`1`/(1 - `1`))))
-			ci <- expit(as.vector(confint(xform, 1, level = level, 
+			ci <- expit(as.vector(confint(xform, 1, level = level,
 				df = df, ...)))
 			rval <- coef(m)[1]
 			attr(rval, "var") <- vcov(m)
@@ -66,29 +66,43 @@ MIsvyciprop <-
 			rval <- coef(m)[1]
 			attr(rval, "var") <- vcov(m)
 			alpha <- 1 - level
-			n.eff <- n.eff * (qt(alpha/2, mean(unlist(lapply(design$designs,nrow))) - 1)/qt(alpha/2, 
+			n.eff <- n.eff * (qt(alpha/2, mean(unlist(lapply(design$designs,nrow))) - 1)/qt(alpha/2,
 				mean(unlist(lapply(design$designs,survey::degf)))))^2
-			ci <- c(qbeta(alpha/2, n.eff * rval, n.eff * (1 - rval) + 
-				1), qbeta(1 - alpha/2, n.eff * rval + 1, n.eff * 
+			ci <- c(qbeta(alpha/2, n.eff * rval, n.eff * (1 - rval) +
+				1), qbeta(1 - alpha/2, n.eff * rval + 1, n.eff *
 				(1 - rval)))
 		}
 		else {
 			m <- eval(bquote(mitools::MIcombine(with(design,svyglm(.(formula[[2]]) ~ 1, family = quasibinomial)))))
 			cimethod <- switch(method, logit = "Wald", likelihood = "likelihood")
-			ci <- suppressMessages(as.numeric(expit(confint(m, 1, 
+			ci <- suppressMessages(as.numeric(expit(confint(m, 1,
 				level = level, method = cimethod, ddf = df))))
 			rval <- expit(coef(m))[1]
 			attr(rval, "var") <- vcov(eval(bquote(mitools::MIcombine(with(design,survey::svymean(~as.numeric(.(formula[[2]])), ...))))))
 		}
 		halfalpha <- (1 - level)/2
-		names(ci) <- paste(round(c(halfalpha, (1 - halfalpha)) * 
+		names(ci) <- paste(round(c(halfalpha, (1 - halfalpha)) *
 			100, 1), "%", sep = "")
 		names(rval) <- deparse(formula[[2]])
 		attr(rval, "ci") <- ci
 		class(rval) <- "svyciprop"
 		rval
 	}
-	
-	
+
+
 # pulled from the R survey library
 expit <- function(eta) exp(eta)/(1 + exp(eta))
+
+# svychisq()  on multiply-imputed data
+MIsvychisq<-function(formula, design , statistic = "Chisq" , ... ) {
+
+  if ( !( statistic %in% c( "Chisq" ) ) ) { stop( " This method is only implemented for `statistic = 'Chisq'`." ) }
+
+  m <- with( design , svychisq( formula , statistic = statistic ) )
+
+  dk  <- as.numeric( sapply( m , FUN = function( x ) x[["statistic"]] ) )
+  df <- as.numeric( sapply( m , FUN = function( x ) x[["parameter"]][ "df" ] ) )
+
+  return( miceadds::micombine.chisquare( dk=dk, df=df[[1]] , display = TRUE , version = 1 ) )
+
+}
