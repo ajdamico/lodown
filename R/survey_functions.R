7d7cb93c6d50baaf8b317fe12e499334063e087d
Anthony Damico <ajdamico@gmail.com>
2017-06-25 08:19:13 -0400

    remove miceadds as a dependency
    this change is a big install time reduction for mac/unix  cc
    @guilhermejacob

--- R/survey_functions.R
+++ R/survey_functions.R
@@ -93,6 +93,40 @@ MIsvyciprop <-
 # pulled from the R survey library
 expit <- function(eta) exp(eta)/(1 + exp(eta))
 
+# pulled from the R miceadds library
+miceadds_micombine.chisquare <-
+	function (dk, df, display = TRUE, version = 1) {
+		M <- length(dk)
+		if (version == 0) {
+			mean.dk <- mean(dk)
+			sdk.square <- stats::var(sqrt(dk))
+			Dval <- (mean.dk/df - (1 - 1/M) * sdk.square)/(1 + (1 + 
+				1/M) * sdk.square)
+			df2 <- (M - 1)/df^(3/M) * (1 + M/(M + 1/M)/sdk.square)^2
+		}
+		if (version == 1) {
+			g2 <- dk
+			m <- length(g2)
+			g <- sqrt(g2)
+			mg2 <- sum(g2)/m
+			r <- (1 + 1/m) * (sum(g^2) - (sum(g)^2)/m)/(m - 1)
+			Dval <- (mg2/df - r * (m + 1)/(m - 1))/(1 + r)
+			df2 <- (m - 1) * (1 + 1/r)^2/df^(3/m)
+		}
+		pval <- stats::pf(Dval, df1 = df, df2 = df2, lower.tail = FALSE)
+		chisq.approx <- Dval * df
+		p.approx <- 1 - stats::pchisq(chisq.approx, df = df)
+		res <- c(D = Dval, p = pval, df = df, df2 = df2)
+		if (display) {
+			cat("Combination of Chi Square Statistics for Multiply Imputed Data\n")
+			cat(paste("Using", M, "Imputed Data Sets\n"))
+			cat(paste("F(", df, ", ", round(df2, 2), ")", "=", round(Dval, 
+				3), "     p=", round(pval, 5), sep = ""), "\n")
+		}
+		invisible(res)
+	}
+
+
 # svychisq()  on multiply-imputed data
 MIsvychisq<-function(formula, design , statistic = "Chisq" , ... ) {
 
@@ -103,6 +137,6 @@ MIsvychisq<-function(formula, design , statistic = "Chisq" , ... ) {
   dk  <- as.numeric( sapply( m , FUN = function( x ) x[["statistic"]] ) )
   df <- as.numeric( sapply( m , FUN = function( x ) x[["parameter"]][ "df" ] ) )
 
-  return( miceadds::micombine.chisquare( dk=dk, df=df[[1]] , display = TRUE , version = 1 ) )
+  return( miceadds_micombine.chisquare( dk=dk, df=df[[1]] , display = TRUE , version = 1 ) )
 
 }
