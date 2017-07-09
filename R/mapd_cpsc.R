d7ecbe2e77243016719b4eac2d24aae3c789ba1b
Anthony Damico <ajdamico@gmail.com>
2017-07-09 09:25:23 -0400

    guess_max

--- R/mapd_cpsc.R
+++ R/mapd_cpsc.R
@@ -57,9 +57,9 @@ lodown_mapd_cpsc <-
 		# your local computer to each of the unzipped files
 		unzipped_files <- unzip_warn_fail( tf , exdir = np_dirname( catalog[ i , 'output_filename' ] ) )
 
-		this_cont <- data.frame( readr::read_csv( grep( "Contract_Info" , unzipped_files , value = TRUE ) ) )
+		this_cont <- data.frame( readr::read_csv( grep( "Contract_Info" , unzipped_files , value = TRUE ) , guess_max = 100000 ) )
 
-		this_enr <- data.frame( readr::read_csv( grep( "Enrollment_Info" , unzipped_files , value = TRUE ) , col_types = 'ccccc' ) )
+		this_enr <- data.frame( readr::read_csv( grep( "Enrollment_Info" , unzipped_files , value = TRUE ) , guess_max = 100000 ) )
 
 		names( this_cont ) <- gsub( "Contract.Number" , "Contract.ID" , names( this_cont ) , fixed = TRUE )
 		names( this_enr ) <- gsub( "Contract.Number" , "Contract.ID" , names( this_enr ) , fixed = TRUE )
