b5fc7cc6ba7c80cacf9c960ebf1a1a5729a6b03d
Anthony Damico <ajdamico@gmail.com>
2017-08-14 02:47:27 -0400

    mapd_benefits draft

---
+++ R/mapd_benefits.R
@@ -0,0 +1,81 @@
+get_catalog_mapd_benefits <-
+  function( data_name = "mapd_benefits" , output_dir , ... ){
+
+	cpsc_url <- "https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/MCRAdvPartDEnrolData/Benefits-Data.html"
+
+	all_dates <- rvest::html_table(xml2::read_html(cpsc_url))
+
+	all_dates <- all_dates[[1]][ , "Report Period"]
+
+	all_links <- rvest::html_nodes(xml2::read_html(cpsc_url),xpath='//td/a')
+
+	prefix <- "https://www.cms.gov/"
+
+	these_links <- 
+		gsub( "\">(.*)" , "" , 
+		gsub( '<a href=\"' , prefix , 
+			all_links ) )
+
+    this_catalog <-
+      data.frame(
+          output_directory = paste0( output_dir , "/" , all_dates ) ,
+          full_url = as.character( these_links ) ,
+          year = all_dates ,
+		  stringsAsFactors = FALSE
+      )
+	
+	for( this_row in seq( nrow( this_catalog ) ) ){
+		
+		link_text <- readLines( this_catalog[ this_row , 'full_url' ] )
+		link_line <- grep( "zip" , link_text , value = TRUE )
+		link_line <- gsub( '(.*) href=\"' , "" , gsub( '(.*) href=\"/' , prefix , link_line ) )
+		this_catalog[ this_row , 'full_url' ] <- gsub( '\">(.*)' , "" , link_line )
+
+	}
+	
+	
+	this_catalog[ order( this_catalog$year ) , ]
+  }
+
+
+lodown_mapd_benefits <-
+	function( data_name = "mapd_benefits" , catalog , ... ){
+
+		tf <- tempfile()
+
+		for ( i in seq_len( nrow( catalog ) ) ){
+
+			# download the file
+			cachaca( catalog[ i , "full_url" ] , tf , mode = 'wb' , filesize_fun = 'httr' )
+
+			unzipped_files <- unzip_warn_fail( tf , exdir = np_dirname( catalog[ i , 'output_directory' ] ) )
+
+			txt_files <- grep( "\\.txt$" , unzipped_files , value = TRUE )
+
+			txt_files <- txt_files[ !grepl( "^readme" , basename( txt_files ) , ignore.case = TRUE ) ]
+
+			txt_files <- sort( txt_files )
+
+			for( j in seq_along( txt_files ) ){
+
+				x <- data.frame( readr::read_delim( txt_files[ j ] , delim = "\t" , guess_max = 10000 ) )
+
+				names( x ) <- tolower( names( x ) )
+
+				saveRDS( x , file = file.path( catalog[ i , 'output_directory' ] , gsub( "\\.txt" , ".rds" , basename( txt_files[ j ] ) ) ) )
+
+				rm( x ) ; gc()
+
+			}
+			
+			file.remove( unzipped_files )
+
+			cat( paste0( data_name , " catalog entry " , i , " of " , nrow( catalog ) , " stored in '" , catalog[ i , 'output_directory' ] , "'\r\n\n" ) )
+
+		}
+
+
+		catalog
+
+	}
+
