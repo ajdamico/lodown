338e6ffa3782444a7b2c880b03f366a6bea467fd
Anthony Damico <ajdamico@gmail.com>
2018-05-04 13:07:29 -0400

    typo

--- R/pirls.R
+++ R/pirls.R
@@ -171,7 +171,7 @@ pirls_rwcf <-
 			z[ z$jkzone == i & z$jkrep == 0 , paste0( 'rw' , i ) ] <- 0
 			
 			z[ z$jkzone == i & z$jkrep == 1 , paste0( 'rw' , i + 75 ) ] <- 0
-			z[ z$jkzone == i & z$jkrep == 0 , paste0( 'rw' , i + 75 ) ] <- z[ z$jkzone == i & z$jkrep == 1 , wgt ] * 2
+			z[ z$jkzone == i & z$jkrep == 0 , paste0( 'rw' , i + 75 ) ] <- z[ z$jkzone == i & z$jkrep == 0 , wgt ] * 2
 			
 		}
 		z[ , paste0( 'rw' , 1:150 ) ]
