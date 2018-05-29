911e7c85838c33de907fa59cce2a04924ab445ce
leoniedu <e.leoni@gmail.com>
2018-05-28 23:45:33 -0300

    fix filename in remove_uf

--- R/pnad.R
+++ R/pnad.R
@@ -279,8 +279,8 @@ pnad_remove_uf <-
 
 		# read the SAS import file into R
 		##does not work on linux sascon <- file( sasfile , "rb" , blocking = FALSE , encoding = "windows-1252" )
-		sas_lines <- readLines( sascon , encoding="windows-1252")
-		if (is.na(sas_lines[1])) sas_lines <- readLines( sascon , encoding="latin1")		      
+		sas_lines <- readLines( sasfile , encoding="windows-1252")
+		if (is.na(sas_lines[1])) sas_lines <- readLines( sasfile , encoding="latin1")		      
 		##close( sascon )
 		
 		sas_lines <- iconv( sas_lines , "" , "ASCII//TRANSLIT" )
