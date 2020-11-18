265d12d08b84fc1b681148001a77bb1522652911
Anthony Damico <ajdamico@gmail.com>
2020-11-18 14:46:01 -0500

    Revert "Update sipp.R"
    This reverts commit a2231ce85bda3067e20526e577890b477bafda89.

--- R/sipp.R
+++ R/sipp.R
@@ -71,11 +71,11 @@ get_catalog_sipp <-
 				db_tablename = c( paste0( "w" , 1:16 ) , paste0( "rw" , 1:16 ) , paste0( "tm" , c( 1:11 , 13 ) ) , "wgtw16" , paste0( 'cy' , 1:5 ) , paste0( 'pn' , 1:5 ) ) ,
 				full_url =
 					c(
-						paste0( "https://www2.census.gov/programs-surveys/sipp/data/datasets/2008/l08puw" , 1:16 , ".zip" ) ,
-						paste0( "https://www2.census.gov/programs-surveys/sipp/data/datasets/2008/rw08w" , 1:16 , ".zip" ) ,
-						paste0( "https://www2.census.gov/programs-surveys/sipp/data/datasets/2008/p08putm" , c( 1:11 , 13 ) , ".zip" ) ,
-						"https://www2.census.gov/programs-surveys/sipp/data/datasets/2008/lgtwgt2008w16.zip" ,
-						paste0( "https://www2.census.gov/programs-surveys/sipp/data/datasets/2008/lrw08" , c( paste0( 'cy' , 1:5 ) , paste0( 'pn' , 1:5 ) ) , ".zip" )
+						paste0( "https://thedataweb.rm.census.gov/pub/sipp/2008/l08puw" , 1:16 , ".zip" ) ,
+						paste0( "https://thedataweb.rm.census.gov/pub/sipp/2008/rw08w" , 1:16 , ".zip" ) ,
+						paste0( "https://thedataweb.rm.census.gov/pub/sipp/2008/p08putm" , c( 1:11 , 13 ) , ".zip" ) ,
+						"https://thedataweb.rm.census.gov/pub/sipp/2008/lgtwgt2008w16.zip" ,
+						paste0( "https://thedataweb.rm.census.gov/pub/sipp/2008/lrw08" , c( paste0( 'cy' , 1:5 ) , paste0( 'pn' , 1:5 ) ) , ".zip" )
 					) ,
 				dbfile = paste0( output_dir , "/SQLite_2008.df" ) ,
 				stringsAsFactors = FALSE
